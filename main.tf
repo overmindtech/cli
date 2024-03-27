c5db6ba655f4db6e31f874e6018f89a582d8c71a
David Schmitt <david.schmitt@overmind.tech>
2024-03-27 14:33:33 +0100

    Create simple terraform example for testing

---
+++ main.tf
@@ -0,0 +1,26 @@
+# This is a very simple example to deploy a few cheap resources into AWS to test the new `terraform plan` and `terraform apply` subcommands.
+
+terraform {
+  required_version = ">= 1.5.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.56"
+    }
+  }
+}
+
+module "bucket" {
+  source  = "terraform-aws-modules/s3-bucket/aws"
+  version = "~> 4.0"
+
+  bucket_prefix = "cli-test"
+
+  control_object_ownership = true
+  object_ownership         = "BucketOwnerEnforced"
+  block_public_policy      = true
+  block_public_acls        = true
+  ignore_public_acls       = true
+  restrict_public_buckets  = true
+}
