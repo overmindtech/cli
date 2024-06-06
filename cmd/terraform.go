d2296c6a96fef55652cc1747545286e319ceb62f
Dylan Ratcliffe <dylan@overmind.tech>
2024-06-06 12:43:28 +0000

    Added a description for the terraform commands
    Fixes #362

--- cmd/terraform.go
+++ cmd/terraform.go
@@ -8,8 +8,19 @@ import (
 var terraformCmd = &cobra.Command{
 	Use:     "terraform",
 	GroupID: "iac",
-	Short:   "Run Terrafrom with Overmind's change tracking - COMING SOON",
-	Long:    `COMING SOON`,
+	Short:   "Run Terrafrom with Overmind's risk analysis and change tracking",
+	Long: `By using 'overmind terraform plan/apply' in place of your normal
+'terraform plan/apply' commands, you can get a risk analysis and change
+tracking for your Terraform changes with no extra effort.
+
+Plan: Overmind will run a normal plan, then determine the potential blast
+radius using real-time data from AWS and Kubernetes. It will then analyse the
+risks that the changes pose to your infrastructure and return them at the
+command line.
+
+Apply: Overmind will do all the same steps as a plan, plus it will take a
+snapshot before and after the actual apply, meaning that you get a diff of
+everything that happened, including any unexpected repercussions.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		_ = cmd.Help()
 	},
@@ -17,14 +28,4 @@ var terraformCmd = &cobra.Command{
 
 func init() {
 	rootCmd.AddCommand(terraformCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// terraformCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// terraformCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
