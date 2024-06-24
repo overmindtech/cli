fa5331fdf4b20e5a6b7838da9622bd9a5f7b72f3
David Schmitt <david.schmitt@overmind.tech>
2024-06-24 12:44:17 +0200

    Capture error message for tracing on the span itself
    `span.RecordError` adds an `exception` span event, which is nive to have
    to see the timing, but awful for aggregation in honeycomb. This also
    adds the most relevant data to the span for easier querying.

--- cmd/root.go
+++ cmd/root.go
@@ -218,7 +218,10 @@ func Execute() {
 			// if printing the error was not requested by the appropriate
 			// wrapper, only record the data to honeycomb and sentry, the
 			// command already has handled logging
-			cmdSpan.SetAttributes(attribute.Bool("ovm.cli.fatalError", true))
+			cmdSpan.SetAttributes(
+				attribute.Bool("ovm.cli.fatalError", true),
+				attribute.String("ovm.cli.fatalError.msg", err.Error()),
+			)
 			cmdSpan.RecordError(err)
 			sentry.CaptureException(err)
 		}
