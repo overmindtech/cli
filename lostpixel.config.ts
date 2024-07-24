855dd60c9db7fc121cd7c16a10a4b50ba91391ab
David Schmitt <david.schmitt@overmind.tech>
2024-07-24 11:12:28 +0200

    Fix lost-pixel config

--- lostpixel.config.ts
+++ lostpixel.config.ts
@@ -3,9 +3,9 @@ import { CustomProjectConfig } from 'lost-pixel';
 export const config: CustomProjectConfig = {
     customShots: {
         currentShotsPath: "./e2e",
+    },
 
-        // Lost Pixel Platform (to use in Platform mode, comment out the OSS mode and uncomment this part )
-        lostPixelProjectId: 'cljcuqix95y2ui10ekiwatpau',
-        apiKey: process.env.LOST_PIXEL_API_KEY,
-    }
+    // Lost Pixel Platform (to use in Platform mode, comment out the OSS mode and uncomment this part )
+    lostPixelProjectId: 'cljcuqix95y2ui10ekiwatpau',
+    apiKey: process.env.LOST_PIXEL_API_KEY,
 };
