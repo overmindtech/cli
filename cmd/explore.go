22f479585d6a14b8088ac66edb2df3fb7cab06b0
David Schmitt <david.schmitt@overmind.tech>
2024-08-14 11:35:12 +0200

    Implement the `explore` command to have sources running
    Run sources locally using terraform's configured authorization to provide data when using https://app.overmind.tech/explore

---
+++ cmd/explore.go
@@ -0,0 +1,140 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"atomicgo.dev/keyboard"
+	"atomicgo.dev/keyboard/keys"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/overmindtech/aws-source/proc"
+	"github.com/overmindtech/cli/tfutils"
+	stdlibsource "github.com/overmindtech/stdlib-source/sources"
+	"github.com/pterm/pterm"
+	"github.com/sourcegraph/conc/pool"
+	"github.com/spf13/cobra"
+)
+
+// exploreCmd represents the explore command
+var exploreCmd = &cobra.Command{
+	Use:    "explore",
+	Short:  "Run local sources for using in the Explore page",
+	Long:   `Run sources locally using terraform's configured authorization to provide data when using https://app.overmind.tech/explore.`,
+	PreRun: PreRunSetup,
+	RunE:   Explore,
+
+	// SilenceErrors: false,
+}
+
+func Explore(cmd *cobra.Command, args []string) error {
+	pterm.Success.Prefix.Text = "✔︎"
+
+	ctx := cmd.Context()
+
+	multi := pterm.DefaultMultiPrinter
+
+	loginSpinner, _ := pterm.DefaultSpinner.WithWriter(multi.NewWriter()).Start("Connecting to Overmind")
+
+	_, _ = multi.Start()
+	defer func() {
+		_, _ = multi.Stop()
+	}()
+
+	ctx, oi, token, err := login(ctx, cmd, []string{"request:receive"})
+	if err != nil {
+		return err
+	}
+
+	loginSpinner.Success("Connected to Overmind")
+
+	stdlibSpinner, _ := pterm.DefaultSpinner.WithWriter(multi.NewWriter()).Start("Starting stdlib source engine")
+	awsSpinner, _ := pterm.DefaultSpinner.WithWriter(multi.NewWriter()).Start("Starting AWS source engine")
+
+	natsOptions := natsOptions(ctx, oi, token)
+
+	p := pool.New().WithErrors()
+
+	p.Go(func() error {
+		stdlibEngine, err := stdlibsource.InitializeEngine(natsOptions, 2_000, true)
+		if err != nil {
+			stdlibSpinner.Fail("Failed to initialize stdlib source engine")
+			return fmt.Errorf("failed to initialize stdlib source engine: %w", err)
+		}
+
+		// todo: pass in context with timeout to abort timely and allow Ctrl-C to work
+		err = stdlibEngine.Start()
+		if err != nil {
+			stdlibSpinner.Fail("Failed to start stdlib source engine")
+			return fmt.Errorf("failed to start stdlib source engine: %w", err)
+		}
+		stdlibSpinner.Success("Stdlib source engine started")
+		return nil
+	})
+
+	p.Go(func() error {
+		tfEval, err := tfutils.LoadEvalContext(args, os.Environ())
+		if err != nil {
+			awsSpinner.Fail("Failed to load variables from the environment")
+			return fmt.Errorf("failed to load variables from the environment: %w", err)
+		}
+
+		providers, err := tfutils.ParseAWSProviders(".", tfEval)
+		if err != nil {
+			awsSpinner.Fail("Failed to parse providers")
+			return fmt.Errorf("failed to parse providers: %w", err)
+		}
+		configs := []aws.Config{}
+
+		for _, p := range providers {
+			if p.Error != nil {
+				// skip providers that had errors. This allows us to use
+				// providers we _could_ detect, while still failing if there is
+				// a true syntax error and no providers are available at all.
+				continue
+			}
+			c, err := tfutils.ConfigFromProvider(ctx, *p.Provider)
+			if err != nil {
+				awsSpinner.Fail("Error when converting provider to config")
+				return fmt.Errorf("error when converting provider to config: %w", err)
+			}
+			configs = append(configs, c)
+		}
+
+		awsEngine, err := proc.InitializeAwsSourceEngine(ctx, natsOptions, 2_000, configs...)
+		if err != nil {
+			awsSpinner.Fail("Failed to initialize AWS source engine")
+			return fmt.Errorf("failed to initialize AWS source engine: %w", err)
+		}
+
+		// todo: pass in context with timeout to abort timely and allow Ctrl-C to work
+		err = awsEngine.Start()
+		if err != nil {
+			awsSpinner.Fail("Failed to start AWS source engine")
+			return fmt.Errorf("failed to start AWS source engine: %w", err)
+		}
+
+		awsSpinner.Success("AWS source engine started")
+		return nil
+	})
+
+	err = p.Wait()
+	if err != nil {
+		return fmt.Errorf("error starting sources: %w", err)
+	}
+
+	pterm.Fprinto(multi.NewWriter(), pterm.Success.Sprint("Press any key to stop the sources"))
+	err = keyboard.Listen(func(keyInfo keys.Key) (stop bool, err error) {
+		return true, nil
+	})
+	if err != nil {
+		return fmt.Errorf("error reading keyboard input: %w", err)
+	}
+
+	return nil
+}
+
+func init() {
+	rootCmd.AddCommand(exploreCmd)
+
+	addAPIFlags(exploreCmd)
+}
--- go.mod
+++ go.mod
@@ -31,6 +31,7 @@ require (
 	github.com/pkg/browser v0.0.0-20240102092130-5ac0b6a4141c
 	github.com/pterm/pterm v0.12.79
 	github.com/sirupsen/logrus v1.9.3
+	github.com/sourcegraph/conc v0.3.0
 	github.com/spf13/cobra v1.8.1
 	github.com/spf13/viper v1.19.0
 	github.com/stretchr/testify v1.9.0
@@ -148,7 +149,6 @@ require (
 	github.com/rivo/uniseg v0.4.7 // indirect
 	github.com/sagikazarmark/locafero v0.4.0 // indirect
 	github.com/sagikazarmark/slog-shim v0.1.0 // indirect
-	github.com/sourcegraph/conc v0.3.0 // indirect
 	github.com/spf13/afero v1.11.0 // indirect
 	github.com/spf13/cast v1.6.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
