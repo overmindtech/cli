9e7a2fa03d319e18475520ac7affa9beb5737741
David Schmitt <david.schmitt@overmind.tech>
2024-09-02 16:41:18 +0200

    Cleanup overmind source source handling
    instead of checking out current `main`, use the version requested in
    the CLI's go.mod file. This keeps unrelated PRs from breaking when
    sources are updated, while still ensuring that renovate PRs updating
    the source references also have to update the generated files.

--- tfutils/types.go
+++ tfutils/types.go
@@ -4,8 +4,8 @@ import (
 	"github.com/overmindtech/sdp-go"
 )
 
-//go:generate go run ../extractmaps.go aws-source
-//go:generate go run ../extractmaps.go k8s-source
+//go:generate bash -c "go run ../extractmaps.go $(go list -m -f '{{.Dir}}' github.com/overmindtech/aws-source)"
+//go:generate bash -c "go run ../extractmaps.go $(go list -m -f '{{.Dir}}' github.com/overmindtech/k8s-source)"
 
 type TfMapData struct {
 	// The overmind type name
