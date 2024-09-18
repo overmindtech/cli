f073e09dfff054aa787b64eeb2e194e78b863491
David Schmitt <david.schmitt@overmind.tech>
2024-09-18 17:17:35 +0200

    Also add our defaults for our custom spinner

--- cmd/pterm.go
+++ cmd/pterm.go
@@ -13,6 +13,7 @@ import (
 	"time"
 
 	"connectrpc.com/connect"
+	"github.com/overmindtech/cli/custerm"
 	"github.com/overmindtech/cli/tracing"
 	"github.com/overmindtech/pterm"
 	"github.com/overmindtech/sdp-go"
@@ -31,6 +32,9 @@ func PTermSetup() {
 
 	pterm.DefaultSpinner.Sequence = []string{"⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏"}
 	pterm.DefaultSpinner.Delay = 80 * time.Millisecond
+
+	custerm.DefaultSpinner.Sequence = []string{"⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏"}
+	custerm.DefaultSpinner.Delay = 80 * time.Millisecond
 
 	// ensure that only error messages are printed to the console,
 	// disrupting bubbletea rendering (and potentially getting overwritten).
